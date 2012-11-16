0bafb2524cce5b36f7855c329a958bba841b4694
Sergio Cruz <dev@sergio.cat>
2012-11-16 04:12:46 +0000

    minor

--- includes/routes.php
+++ includes/routes.php
@@ -1,17 +1,24 @@
 <?php
 
-if(!isset($_ROUTES) || empty($_ROUTES)) die('Failure routes.');
+if (!isset($_ROUTES) || empty($_ROUTES)) {
+	die('Failure routes.');
+}
 
 function _u() {
 	global $_ROUTES;
 	$args = func_get_args();
-	if(array_key_exists($args[0], $_ROUTES)) {
-		$url = '';
-		if(defined('BASE')) $url .= BASE;
-		if(!defined('REWRITE') || REWRITE !== true) $url .= 'index.php/';
-		$url .= preg_replace("!(%|{)([0-9]+)(%|})!e", '$args[\\2]', $_ROUTES[$args[0]][0]);
-		return $url;
+	if (!array_key_exists($args[0], $_ROUTES)) {
+		return '';
 	}
+	$url = '';
+	if (defined('BASE')) {
+		$url .= BASE;
+	}
+	if (!defined('REWRITE') || REWRITE !== true) {
+		$url .= 'index.php/';
+	}
+	$url .= preg_replace("!(%|{)([0-9]+)(%|})!e", '$args[\\2]', $_ROUTES[$args[0]][0]);
+	return $url;
 }
 
 if(defined('REWRITE') && REWRITE === true) {
@@ -22,13 +29,14 @@ if(defined('REWRITE') && REWRITE === true) {
 $_ROUTE = urldecode($_ROUTE);
 $_ROUTE = substr($_ROUTE, 1);
 
-foreach($_ROUTES as $k=>$v) {
-	$pattern = preg_replace("!%(.*?)%!", '([0-9]+)', $v[0]);
-	$pattern = preg_replace("!{(.*?)}!", '(.+?)', $pattern);
-	if(preg_match('!^'.$pattern.'$!', $_ROUTE, $matches)) {
-		parse_str(preg_replace("!\{([0-9]+)\}!e", '$matches[\\1]', $v[1]), $_GET);
+foreach ($_ROUTES as $k=>$v) {
+	$pattern = $v[0];
+	$pattern = preg_replace('!%(.*?)%!', '([0-9]+)', $pattern);
+	$pattern = preg_replace('!{(.*?)}!', '(.+?)', $pattern);
+	$pattern = sprintf('!^%s$!', $pattern);
+	if (preg_match($pattern, $_ROUTE, $matches)) {
+		parse_str(preg_replace('!\{([0-9]+)\}!e', '$matches[\\1]', $v[1]), $_GET);
 		$_FOUND = $k;
 	}
 }
 
-?>
